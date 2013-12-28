c72f8815c3e05448489e648b8d060fea166099b3
roshow <rolando@roshow.net>
2013-12-28 13:39:03 -0500

    renamed app.js file; added restify to package.json; lots of other updates to everything, basically.

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
-CONFIG = {
-    mongo: {
-        uri: 'mongodb://you:comein@ds043348.mongolab.com:43348/cmxcanvas'
-    },
-    port: process.env.PORT || 5000
-}
-
-var express = require('express'),
-    handler = require('./handler').handler,
-    app = express(),
-    _port = CONFIG.port;
-
-app.use(express.static(__dirname + '/www'));
-
-app.listen(_port);
-app.get('/getcmxjson/:id', handler.getcmxjson);
-app.get('/getallcmx', handler.getallcmx);
-app.get('*', function(req, res){
-    res.send('404, friends.');
+var restify = require('restify'),
+    db = require('./db_cmx.js').db_cmx,
+    server = restify.createServer({ name: 'my-api' });
+ 
+server.listen(3000, function () {
+  console.log('%s listening at %s', server.name, server.url);
 });
 
-console.log('cmxcanvas-backend listening on port ' + _port);
+server
+  .use(restify.fullResponse())
+  .use(restify.bodyParser());
+
+var util = require('util');
+function NoMatches(message) {
+  restify.RestError.call(this, {
+    restCode: 'NoMatches',
+    statusCode: 411,
+    message: message || 'Nothing on this, babe.',
+    constructorOpt: NoMatches
+  });
+  this.name = 'NoMatches';
+};
+util.inherits(NoMatches, restify.RestError);
+
+server.get('/hello/:name', function(req, res, next) {
+  return next(new NoMatches('not feeling you.'));
+  // return next(new restify.ConflictError('not feeling you.'));
+});
+
+server.get('/comics', function (req, res, next) {
+  db.comics.get({}, function (error, comics) {
+    res.send(comics);
+  });
+});
+server.get('/comics/:id', function (req, res, next) {
+  db.cmxJSON.get({ _id: req.params.id + '_cmxjson' }, function(error, cmxjson) {        
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
+        if (cmxjson) {        
+            console.log(cmxjson);
+            db.comics.get({ _id: req.params.id }, function (error, comic) {
+                if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
+                if (comic) {
+                    comic[0].cmxJSON = cmxjson.JSON;
+                    res.send(comic[0]);
+                }
+                else return next(new restify.InvalidArgumentError('NoMatches'));
+            });
+        } 
+        else return next(new restify.InvalidArgumentError('NoMatches'));
+        // else res.send(404);
+    });  
+});
+server.get('/cmxjson/:id', function (req, res, next) {
+    db.cmxJSON.get({ _id: req.params.id }, function(error, cmxjson) {
+        if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)));
+        if (cmxjson) {
+            res.send(cmxjson);
+        }
+        else return next(new NoMatches());
+    });    
+});
--- db_cmx.js
+++ db_cmx.js
@@ -1,36 +1,24 @@
-var db = require('mongojs').connect(CONFIG.mongo.uri, ['cmxMetaData', 'cmxJSON', 'comics']);
+var db = require('mongojs').connect('mongodb://you:comein@ds043348.mongolab.com:43348/cmxcanvas', ['cmxMetaData', 'cmxJSON', 'comics']);
 
 exports.db_cmx = (function() {
     var db_cmx = {
-        issues: {
-            get: function(q, cb) {
-                db.comics.find(q, function(e, r) {
-                    if (!e){
-                        cb && cb(r);
-                        return r;
-                    }
-                });
-            }
-        },
-
-        metadata: {
+        comics: {
             get: function(q, cb) {
                 db.cmxMetaData.find(q, function(e, r) {
-                    if (!e){
-                        cb && cb(r);
-                        return r;
-                    }
+                    cb && cb(e, r);
+                });
+            },  
+            getOne: function(q, cb) {
+                db.cmxMetaData.find(q, function(e, r) {
+                    cb && cb(e, r[0]);
                 });
             }
         },
 
-        cmxjson: {
+        cmxJSON: {
             get: function(q, cb) {
                 db.cmxJSON.find(q, function(e, r) {
-                    if (!e){
-                        cb && cb(r);
-                        return r;
-                    }
+                    cb && cb(e, r[0]);
                 });
             },
             set: function(q, u, cb) {
