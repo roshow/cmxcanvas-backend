e1903aa7ffb7b6a45e6c981f002906913e1c0c73
roshow <rolando@roshow.net>
2014-08-07 00:00:42 -0400

    moved route methods to handler, renamed mongoose.js module to db.js since that's how I refer to it everywhere else

--- db.js
+++ db.js
@@ -1,39 +1,130 @@
-var mongoConf = require('nconf')
-                    .file({ file: 'conf/heroku.json' })
-                    .get('mongo'),
-    db = require('mongojs').connect(mongoConf.uri, mongoConf.collections);
+/* globals require */
 
-module.exports = (function() {
-    var db_cmx = {
-        comics: {
-            get: function(q, cb) {
-                db.cmxMetaData.find(q, function(e, r) {
-                    cb && cb(e, r);
-                });
-            },  
-            findOne: function(q, cb) {
-                db.cmxMetaData.find(q, function(e, r) {
-                    cb && cb(e, r[0]);
-                });
-            }
-        },
+'use strict';
 
-        cmxJSON: {
-            get: function(q, cb) {
-                db.cmxJSON.find(q, function(e, r) {
-                    cb && cb(e, r[0]);
-                });
+var mongoose = require('mongoose'),
+    promised = require('promised-io/promise'),
+    ro = require('./routils'),
+    db = {},
+    conf;
+
+
+conf = {
+    db: {
+        user: process.env.CBDB_USER,
+        pw: process.env.CBDB_PW,
+        models: [
+            {
+                collection: 'cmxMetaData',
+                schema: require('./metadataSchema'),
             },
-            set: function(q, u, cb) {
-                db.users.findAndModify({
-                    query: q,
-                    update: u,
-                    new: true
-                }, function(e, r) {
-                    cb && cb(r);
+            {
+                collection: 'cmxJSON',
+                schema: require('./viewSchema'),
+            }
+        ]
+    }
+};
+if (process.env.CANVASBOOK_ENV === 'staging'){
+    ro.log('using staging db');
+    conf.db.host = 'ds053439.mongolab.com';
+    conf.db.database = 'canvasbookstaging';
+    conf.db.port = '53439';
+}
+else {
+    ro.log('connecting to production');
+    conf.db.host = 'ds043348.mongolab.com';
+    conf.db.database = 'cmxcanvas';
+    conf.db.port = '43348';
+}
+var dbModels = {};
+conf.db.models.forEach(function (model){
+    var modelSchema = mongoose.Schema(model.schema, { collection: model.collection }),
+        modelName = model.name || model.collection; 
+    dbModels[modelName] = mongoose.model(modelName, modelSchema);
+});
+
+
+db.find = function(model, query){
+    // ro.log('finding:', query, 'in:', model);
+    var deferred = new promised.Deferred();
+    dbModels[model].find(query || {}, function (err, docs){
+        // console.log(arguments);
+        if (err) { deferred.reject(err); }
+        else { deferred.resolve(docs); }
+    });
+    return deferred;
+};
+
+db.put = function(modelName, model){
+    var def = new promised.Deferred();
+    if (model._id){
+        var _id = model._id;
+        delete model._id;
+        dbModels[modelName].findOneAndUpdate({ _id: _id }, model, { upsert : true }, function(err, updatedModel){
+            if (err) {
+                def.reject(err);
+            }
+            else {
+                def.resolve(updatedModel);
+            }
+        });
+    }
+    else {
+        dbModels[modelName].create(model, function(err, savedModel){
+            if (err) {
+                def.reject(err);
+            }
+            else {
+                def.resolve(savedModel);
+            }
+        });
+    }
+    return def;
+};
+
+db.override = function(modelName, model){
+    var def = new promised.Deferred();
+    if (model._id){
+        dbModels[modelName].findOneAndRemove({ _id: model._id }, function(err){
+            if (err) {
+                def.reject(err);
+            }
+            else {
+                dbModels[modelName].create(model, function (err, savedModel){
+                    def.resolve(savedModel);
                 });
             }
-        }
-    };
-    return db_cmx;
-}());
+        });
+    }
+    else {
+        dbModels[modelName].create(model, function (err, savedModel){
+            def.resolve(savedModel);
+        });
+    }
+    return def;
+};
+
+db.putABunch = function(models){
+    var that = this,
+        allthosepromises = [];
+    models.forEach(function (model){
+        allthosepromises.push(that.update(model));
+    });
+    return promised.all(allthosepromises);
+};
+
+db.connect = function(){
+    var deferred = new promised.Deferred();
+    mongoose.connect('mongodb://' + conf.db.user + ':' + conf.db.pw +  '@' + conf.db.host + ':' + conf.db.port + '/' + conf.db.database);
+    mongoose.connection
+        .on('error', console.error.bind(console, 'connection error:'))
+        .once('open', deferred.resolve);
+    return deferred;
+};
+
+db.disconnect = function(){
+    mongoose.disconnect();
+};
+
+module.exports = db;
